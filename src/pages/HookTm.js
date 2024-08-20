0fdc5acf59a63ab2a21696f075824f80b98c618a
sagarshrestha81 <sagarcrestha81@gmail.com>
2024-08-20 07:27:41 +0545

    image

--- src/pages/HookTm.js
+++ src/pages/HookTm.js
@@ -2,52 +2,50 @@ import axios, { all } from "axios";
 import { useEffect, useState } from "react";
 export default function HookTm() {
   let [num, setNum] = useState(null);
-  let [allDog,setAllDog] = useState([]);
+  let [allDog, setAllDog] = useState([]);
 
-  
-  const getData =async ()=>{
+  const getData = async () => {
     const options = {
-      method: 'GET',
-      url: 'https://dog.ceo/api/breeds/list/all',
-    };  
-    const responce=await axios.request(options);
-    // console.log(responce.data.message)
-    setAllDog(responce.data.message)
-  
-  }
-  const getDogImage = async (dogName)=>{
-console.log(dogName)
-  }
+      method: "GET",
+      url: "https://dog.ceo/api/breeds/list/all",
+    };
+    const res = await axios.request(options);
+    setAllDog(res.data.message);
+  };
+  const getDogImage = async (dogName) => {
+    // console.log(dogName);
+
+    const options = {
+      method: "GET",
+      url: "https://dog.ceo/api/breed/affenpinscher/images/random",
+    };
+    const resRandom = await axios.request(options);
+    console.log(resRandom);
+
+  };
 
   useEffect(() => {
     getData();
   }, []);
 
-  let arr = [1, 2, 3, 4, 5];
-
-
-
-
 
   const DataList = () => {
-
-  //   console.log(allDog)
-
-  //   Object.keys(allDog).map((data,idx)=>{
-  //   console.log(data)
-  //  })
- 
     return (
       <>
-         {Object.keys(allDog).map((data, idx) => {
+        {Object.keys(allDog).map((data, idx) => {
           return (
             <div className="col-sm-4" key={idx}>
-              <div className="card-single card p-2 mb-2" onClick={()=>{getDogImage(data)}}>
-                <h3 className="card-title" >{data}</h3>
+              <div
+                className="card-single card p-2 mb-2"
+                onClick={() => {
+                  getDogImage(data);
+                }}
+              >
+                <h3 className="card-title">{data}</h3>
               </div>
             </div>
           );
-        })} 
+        })}
       </>
     );
   };
